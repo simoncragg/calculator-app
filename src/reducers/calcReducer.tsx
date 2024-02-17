5cb5268c05eebdd6cb88e2bb13e847aa28f46f67
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2024-02-17 23:08:09 +0000

    refactor: small reducer code refactorings

--- src/reducers/calcReducer.tsx
+++ src/reducers/calcReducer.tsx
@@ -49,7 +49,7 @@ function allClear() {
     lastInput: undefined,
     lastOperand: undefined,
     lastOperation: undefined,
-    output: "0"
+    output: "0",
   };
 }
 
@@ -60,46 +60,53 @@ function clear(calc: CalcState) {
     lastInput: "AC",
     lastOperand: undefined,
     lastOperation: undefined,
-    output: "0"
+    output: "0",
   };
 }
 
 function invertNumber(calc: CalcState) {
   if (calc.lastInput === "=" && calc.output === "Error") return calc;
+
   const invertedNumber = parseFloat(calc.currentOperand) * -1;
   const strInvertedNumber = invertedNumber.toString();
+  const output = formatNumberString(strInvertedNumber, { maxDigits: MAX_DIGITS });
+
   return {
     ...calc,
     currentOperand: strInvertedNumber,
     lastInput: INVERT_SYMBOL,
-    output: formatNumberString(strInvertedNumber, { maxDigits: MAX_DIGITS })
+    output,
   };
 }
 
 function calculatePercent(calc: CalcState) {
   if (calc.lastInput === "=" && calc.output === "Error") return calc;
+
   const percentResult = parseFloat(calc.currentOperand) / 100;
   const strPercentResult = percentResult.toString();
+  const output = formatNumberString(strPercentResult, { maxDigits: MAX_DIGITS });
+
   return {
     ...calc,
     currentOperand: strPercentResult,
     lastInput: "%",
-    output: formatNumberString(strPercentResult, { maxDigits: MAX_DIGITS })
+    output,
   };
 }
 
 function updateCurrentOperand (calc: CalcState, digit: string) {
   if (calc.currentOperand.replace(".", "").length === MAX_DIGITS) return calc;
   if (digit === "." && calc.currentOperand.includes(".")) return calc;
-  
+
   const isFirstDigit = calc.currentOperand === "0" && digit !== "." || calc.lastInput === "=";
   const currentOperand = isFirstDigit ? digit : calc.currentOperand + digit;
+  const output = formatNumberString(currentOperand, { maxDigits: MAX_DIGITS });
 
   return {
     ...calc,
     currentOperand,
     lastInput: digit,
-    output: formatNumberString(currentOperand, { maxDigits: MAX_DIGITS })
+    output,
   };
 }
 
@@ -128,25 +135,30 @@ function updateExpression (calc: CalcState, newOperator: string): CalcState {
 
 function evaluateExpression (calc: CalcState) {
   if (calc.lastInput === "=") return repeatLastOperation(calc);
+
   const expression = calc.expression + calc.currentOperand;
   const result = evaluate(expression).toString();
   const lastOperation = getLastOperation(calc.expression, calc.currentOperand);
+  const output = formatNumberString(result, { maxDigits: MAX_DIGITS, useRounding: true });
+
   return {
     ...calc,
     currentOperand: result,
     expression: "",
     lastOperation,
     lastInput: "=",
-    output: formatNumberString(result, { maxDigits: MAX_DIGITS, useRounding: true })
+    output,
   };
 }
 
 function repeatLastOperation(calc: CalcState) {
   const result = evaluate(calc.currentOperand + calc.lastOperation).toString();
+  const output = formatNumberString(result, { maxDigits: MAX_DIGITS, useRounding: true });
+
   return {
     ...calc,
     currentOperand: result,
-    output: formatNumberString(result, { maxDigits: MAX_DIGITS, useRounding: true })
+    output,
   };
 }
 
@@ -155,6 +167,7 @@ function getLastOperation(expression: string, currentOperand: string) {
   const lastOperation = (lastOperator)
     ? expression.substring(i) + currentOperand
     : "";
+
   return lastOperation;
 }
 
@@ -164,14 +177,17 @@ function buildOutputForNewOperator(operand: string, expression: string, newOpera
     const evaluation = evaluate(expression.substring(evaluationIndex));
     return formatNumberString(evaluation.toString(), { maxDigits: MAX_DIGITS });
   }
+
   return formatNumberString(operand, { maxDigits: MAX_DIGITS });
 }
 
 function getEvaluationIndex(expression: string, newOperator: string) {
   const {lastOperator, i} = getLastOperator(expression);
+
   if (!lastOperator) return -1;
   if (isDivideOrMultiply(lastOperator) && isDivideOrMultiply(newOperator)) return i-1;
   if (isAddOrSubtract(lastOperator) && isAddOrSubtract(newOperator)) return 0;
+
   return -1;
 }
 
@@ -181,6 +197,7 @@ function getLastOperator(expression: string) {
       return {lastOperator: expression[i], i};
     }
   }
+  
   return {undefined, i: -1};
 }
 
