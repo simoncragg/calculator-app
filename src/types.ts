98394ca66f1903af31901d65b105080b644dd55a
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2024-03-25 21:37:20 +0000

    refactor: terminate interface properties with semicolons

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,9 @@
 export interface CalcState {
-  currentOperand: string,
-  expression: string[],
-  lastInput?: string,
+  currentOperand: string;
+  expression: string[];
+  lastInput?: string;
   lastOperand?: string;
-  lastOperation?: string,
+  lastOperation?: string;
   output: string;
   voltageLevel: number;
 }
@@ -26,6 +26,6 @@ export interface AdjustVoltagePayload {
 }
 
 export interface GetLastOperatorResultType {
-  lastOperator: string | undefined,
-  index: number
+  lastOperator: string | undefined;
+  index: number;
 };
